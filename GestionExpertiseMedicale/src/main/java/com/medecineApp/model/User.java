fd72297782028caadf4221c1a6a4d6ac4d86715b
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-10-08 17:00:00 +0100

    l'implémentation de la classe utilisateur

---
+++ GestionExpertiseMedicale/src/main/java/com/medecineApp/model/User.java
@@ -0,0 +1,99 @@
+package com.medecineApp.model;
+
+import com.medecineApp.enums.Role;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")  // Adaptez selon votre nom de table
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nom")
+    private String nom;
+
+    @Column(name = "prenom")
+    private String prenom;
+
+    @Column(name = "email")
+    private String email;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    // Constructeur par défaut (obligatoire pour JPA)
+    public User() {
+    }
+
+    // Constructeur avec paramètres
+    public User(String nom, String prenom, String email) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+    }
+
+    // Getters et Setters (TRÈS IMPORTANT : respect de la convention JavaBean)
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Optionnel : toString pour le débogage
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
