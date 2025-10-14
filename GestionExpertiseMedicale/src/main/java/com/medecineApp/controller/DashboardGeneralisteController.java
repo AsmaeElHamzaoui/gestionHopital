1ef12abacb0edf00094c1a476bd2f2bb32f3a77b
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-10-14 21:57:52 +0100

    l'passer les dcspecialistes   la page dashboard-generaliste

--- GestionExpertiseMedicale/src/main/java/com/medecineApp/controller/DashboardGeneralisteController.java
+++ GestionExpertiseMedicale/src/main/java/com/medecineApp/controller/DashboardGeneralisteController.java
@@ -2,8 +2,10 @@ package com.medecineApp.controller;
 
 import com.medecineApp.model.Consultation;
 import com.medecineApp.model.Patient;
+import com.medecineApp.model.User;
 import com.medecineApp.service.ConsultationService;
 import com.medecineApp.service.PatientService;
+import com.medecineApp.service.UserService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -17,13 +19,16 @@ import java.util.List;
 public class DashboardGeneralisteController extends HttpServlet {
     private final PatientService patientService = new PatientService();
     private final ConsultationService consultationService= new ConsultationService();
+    private final UserService userService=new UserService();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // Fetch all patients
         List<Patient> patients = patientService.getAllPatients();
         List<Consultation>  consultations=consultationService.getAllConsultations();
+        List<User> specialistes=userService.getAllSpecialiste();
         req.setAttribute("patients", patients);
         req.setAttribute("consultations", consultations);
+        req.setAttribute("specialistes",specialistes);
 
 
         // Forward to the JSP
