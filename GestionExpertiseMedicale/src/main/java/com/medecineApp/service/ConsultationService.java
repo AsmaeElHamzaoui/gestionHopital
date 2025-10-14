f96d82cb3b2ba736834ce6ec3da3cf757673fded
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-10-14 21:48:36 +0100

    l'ajout de la methode getallConsultations à la mservice des consultations

--- GestionExpertiseMedicale/src/main/java/com/medecineApp/service/ConsultationService.java
+++ GestionExpertiseMedicale/src/main/java/com/medecineApp/service/ConsultationService.java
@@ -1,14 +1,29 @@
 package com.medecineApp.service;
 
+import com.medecineApp.enums.StatutConsultation;
 import com.medecineApp.model.Consultation;
+import com.medecineApp.model.Patient;
 import com.medecineApp.repository.ConsultationRepository;
+import com.medecineApp.util.JpaUtil;
+import jakarta.persistence.EntityManager;
+
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ConsultationService {
     private final ConsultationRepository repo = new ConsultationRepository();
 
     public List<Consultation> getAllConsultationsByPatientId(Long patientId) {
         return repo.findAllByPatientId(patientId);
+    }
+
+    public List<Consultation> getAllConsultations() {
+        return repo.findAll()
+                .stream()
+                .filter(c ->c.getStatut().equals(StatutConsultation.EN_ATTENTE_AVIS_SPECIALISTE)) // filtre par statut
+                .sorted(Comparator.comparing(Consultation::getDateConsultation)) // tri croissant par date
+                .collect(Collectors.toList());
     }
 
     public void saveConsultation(Consultation c) {
