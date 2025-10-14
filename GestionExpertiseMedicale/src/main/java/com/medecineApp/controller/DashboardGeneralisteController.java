0922de9df10335fb4342cf62e4de1b7d580cf571
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-10-15 00:08:43 +0100

    l'apassé les créneaux à la vue du dashboarb generaliste

--- GestionExpertiseMedicale/src/main/java/com/medecineApp/controller/DashboardGeneralisteController.java
+++ GestionExpertiseMedicale/src/main/java/com/medecineApp/controller/DashboardGeneralisteController.java
@@ -1,9 +1,11 @@
 package com.medecineApp.controller;
 
 import com.medecineApp.model.Consultation;
+import com.medecineApp.model.CreneauJournalier;
 import com.medecineApp.model.Patient;
 import com.medecineApp.model.User;
 import com.medecineApp.service.ConsultationService;
+import com.medecineApp.service.CreneauJournalierService;
 import com.medecineApp.service.PatientService;
 import com.medecineApp.service.UserService;
 import jakarta.servlet.ServletException;
@@ -20,15 +22,18 @@ public class DashboardGeneralisteController extends HttpServlet {
     private final PatientService patientService = new PatientService();
     private final ConsultationService consultationService= new ConsultationService();
     private final UserService userService=new UserService();
+    private final CreneauJournalierService creneauJourService=new CreneauJournalierService();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // Fetch all patients
         List<Patient> patients = patientService.getAllPatients();
         List<Consultation>  consultations=consultationService.getAllConsultations();
         List<User> specialistes=userService.getAllSpecialiste();
+        List<CreneauJournalier> creneaux=creneauJourService.getCreneauxSpecialisteNonActif();
         req.setAttribute("patients", patients);
         req.setAttribute("consultations", consultations);
         req.setAttribute("specialistes",specialistes);
+        req.setAttribute("creneaux",creneaux);
 
 
         // Forward to the JSP
