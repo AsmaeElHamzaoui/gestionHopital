2aa559e8ca25eeb63234aa1b9f9194c18e836fd2
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-10-14 21:55:07 +0100

    l'passer les dconsultations filtrées à la page dashboard-generaliste

--- GestionExpertiseMedicale/src/main/java/com/medecineApp/controller/DashboardGeneralisteController.java
+++ GestionExpertiseMedicale/src/main/java/com/medecineApp/controller/DashboardGeneralisteController.java
@@ -1,6 +1,8 @@
 package com.medecineApp.controller;
 
+import com.medecineApp.model.Consultation;
 import com.medecineApp.model.Patient;
+import com.medecineApp.service.ConsultationService;
 import com.medecineApp.service.PatientService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -14,12 +16,15 @@ import java.util.List;
 @WebServlet("/dashboard-generaliste")
 public class DashboardGeneralisteController extends HttpServlet {
     private final PatientService patientService = new PatientService();
-
+    private final ConsultationService consultationService= new ConsultationService();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // Fetch all patients
         List<Patient> patients = patientService.getAllPatients();
+        List<Consultation>  consultations=consultationService.getAllConsultations();
         req.setAttribute("patients", patients);
+        req.setAttribute("consultations", consultations);
+
 
         // Forward to the JSP
         req.getRequestDispatcher("/pages/dashboard-generaliste.jsp").forward(req, resp);
